5838a15daebd67db876728d58d9720410c027bcc
Nemoo0 <damien.royer140@gmail.com>
2019-11-05 11:46:48 +0100

    Cleaning project

--- src/main/java/com/ntd/datasmap/TestController.java
+++ src/main/java/com/ntd/datasmap/TestController.java
@@ -1,6 +1,5 @@
 package com.ntd.datasmap;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- src/main/java/com/ntd/datasmap/models/User.java
+++ src/main/java/com/ntd/datasmap/models/User.java
@@ -5,8 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.springframework.web.bind.annotation.RequestBody;
-
 @Entity
 public class User {
 
